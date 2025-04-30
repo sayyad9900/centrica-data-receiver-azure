140ba2947255d08694010668c53680ab06181cb9
sayyad9900 <sayyad.azharuddin@etaplus.energy>
2025-04-30 13:28:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ def health_check():
     return jsonify({"status": "healthy"}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
